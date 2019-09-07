a039157cd49597cd7e1b8037249079457fb03369
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-09-07 03:04:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,8 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly = {:railroads => {}}
-  monopoly = [:railroads][:pieces]
+  monopoly[:railroads][:pieces]
+  monopoly
 end
 
 def monopoly_with_third_tier
