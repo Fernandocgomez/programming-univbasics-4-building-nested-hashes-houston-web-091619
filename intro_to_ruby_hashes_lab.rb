1deeb8eab96669c2b17de1e543fad095f3161203
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-09-07 03:51:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
       :three_pieces_owned=>100,
       :four_pieces_owned=>200}
   monopoly
-  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:names] = {:reading_railroad=>{}, }
   monopoly
   
   
